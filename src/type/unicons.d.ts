8789f5ba710d5d781b9f42acf1ea09eabfa9091e
SangTranTamLy <sangchaubr089@gmail.com>
2026-03-17 10:16:31 +0700

    fix unicons types

--- src/type/unicons.d.ts
+++ src/type/unicons.d.ts
@@ -1,9 +1,1 @@
-declare module '@iconscout/react-unicons' {
-  import * as React from 'react';
-
-  export const UilMicrophone: React.FC<any>;
-  export const UilBookOpen: React.FC<any>;
-  export const UilHeadphones: React.FC<any>;
-  export const UilFileAlt: React.FC<any>;
-  export const UilArrowRight: React.FC<any>;
-}
+declare module '@iconscout/react-unicons';
